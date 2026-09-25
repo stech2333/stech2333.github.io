f54273ffbc359b3afb672a236523edb8a6dfe4c0
stech2333 <56077247+stech2333@users.noreply.github.com>
2026-09-25 17:26:25 +0800

    Connect private drafts page to Supabase project

--- js/drafts-config.js
+++ js/drafts-config.js
@@ -1,6 +1,5 @@
 // Public project settings only. Never place a password or service_role key here.
 window.DRAFTS_CONFIG = {
-  url: '',
-  publishableKey: ''
+  url: 'https://hvstiyymhgidzacwtwen.supabase.co',
+  publishableKey: 'sb_publishable_4SfJiY51ZWQVFfm-aqpxUg_tGXN3bDO'
 };
-
